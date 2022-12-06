28c31e1ef9039abb23c4c9205420aed33b9398d0
Fredrik Hagebring <frehage@users.noreply.github.com>
2022-12-06 08:14:54 +0100

    Finishing day 6

---
+++ 6/Program.cs
@@ -0,0 +1,3 @@
+﻿string data = System.IO.File.ReadAllText("data.txt");
+Console.WriteLine("Part one: " + Enumerable.Range(4, data.Length).First(i => data[(i - 4)..i].Distinct().Count() == 4));
+Console.WriteLine("Part two: " + Enumerable.Range(14, data.Length).First(i => data[(i - 14)..i].Distinct().Count() == 14));
